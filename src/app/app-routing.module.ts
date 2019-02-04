7497f8863b54f61a3527a8a2ba8908a361ea3c5e
Yogaranjan Das <Yogaranjand@Portal01.nextgen.com>
2019-02-04 14:27:15 +0530

    Fixed module issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,39 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './_guard/auth-guard';
 
-// const routes: Routes = [
-//   {
-//     path: '',
-//     canActivate:[AuthGuard],
-//     redirectTo: 'home',
-//     pathMatch: 'full'
-//   },
-//   {
-//     path: 'home',
-//     canActivate:[AuthGuard],
-//     loadChildren: './home/home.module#HomePageModule'
-//   },
-//   {
-//     path: 'list',
-//     canActivate:[AuthGuard],
-//     loadChildren: './list/list.module#ListPageModule'
-//   },
-//   {
-//     path: 'login',
-//     loadChildren: './login/login.module#LoginPageModule'
-//   },
-//   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-//   { path: 'logout', loadChildren: './logout/logout.module#LogoutPageModule' },
-//   { path: 'employees', loadChildren: './employees/employees.module#EmployeesPageModule' },
-//   { path: 'drivers', loadChildren: './drivers/drivers.module#DriversPageModule' },
-//   { path: 'driverslist', loadChildren: './drivers/driverslist/driverslist.module#DriverslistPageModule' },
-//   { path: 'adddriver', loadChildren: './drivers/adddriver/adddriver.module#AdddriverPageModule' },
-//   { path: 'myprofile', loadChildren: './myprofile/myprofile.module#MyprofilePageModule' },
-//   { path: 'location', loadChildren: './myprofile/location/location.module#LocationPageModule' }
-
-// ];
-
-
 const routes: Routes = [
   {
     path: '',
@@ -62,9 +29,11 @@ const routes: Routes = [
   { path: 'drivers', loadChildren: './drivers/drivers.module#DriversPageModule' },
   { path: 'driverslist', loadChildren: './drivers/driverslist/driverslist.module#DriverslistPageModule' },
   { path: 'adddriver', loadChildren: './drivers/adddriver/adddriver.module#AdddriverPageModule' },
-  { path: 'emplist', loadChildren: './Employees/emplist/emplist.module#EmplistPageModule' },
-  { path: 'addemp', loadChildren: './Employees/addemp/addemp.module#AddempPageModule' }
+  { path: 'myprofile', loadChildren: './myprofile/myprofile.module#MyprofilePageModule' },
+  { path: 'location', loadChildren: './myprofile/location/location.module#LocationPageModule' }
+
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
