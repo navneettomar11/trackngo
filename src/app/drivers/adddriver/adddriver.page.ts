a3357b2051dbfd94e8132b0bd64ea799dc1542a7
Mahil K R <Mahilkr@nextgen.com>
2019-02-05 09:29:56 +0530

    Added successfully saved label

--- src/app/drivers/adddriver/adddriver.page.ts
+++ src/app/drivers/adddriver/adddriver.page.ts
@@ -27,8 +27,10 @@ export class AdddriverPage implements OnInit {
   }
   logForm() {
     this.addDriver.value.type = 'driver';
+    this.addDriver.value.success = ' ';
     this.dataToStore = this.addDriver.value;
     this.storage.set('object', this.dataToStore).then((successData) => {
+      this.addDriver.value.success = 'Successfully Saved!'
       console.log('Data Stored');
       console.log(successData);
     });
@@ -36,6 +38,5 @@ export class AdddriverPage implements OnInit {
   }
 
   ngOnInit() {
-
   }
 }
